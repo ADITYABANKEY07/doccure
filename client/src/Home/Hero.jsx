c5dca78717c30fafb01449d245a9e8baeec0fd36
bankeyaditya <bankeyaditya7@gmail.com>
2026-01-23 12:43:19 +0530

    button modify in mobile

--- client/src/Home/Hero.jsx
+++ client/src/Home/Hero.jsx
@@ -154,20 +154,10 @@ const Hero = () => {
             <div className="leftelem w-[55vw] md:w-[40vw] max-w-3xl mx-auto md:mt-10">
               <div className="">
                 {/* Section 1: Search Input */}
-                {/* <div className="flex-[2] flex items-center px-3 md:px-4 gap-3 w-full md:w-auto">
-                  <i className="ri-hospital-line text-[#002b5b] text-xl"></i>
-                  <input
-                    type="text"
-                    placeholder="Search doctors, clinics, hospitals..."
-                    className="w-full outline-none text-gray-500 placeholder-gray-400 text-sm md:text-base bg-transparent"
-                  />
-                </div> */}
-
-
 
                 {/* Search Button */}
                 <button className="w-full mt-1 md:mt-0 bg-gradient-to-r from-primary to-secondary text-white px-10 py-10 md:px-8 md:py-7 rounded-full flex items-center justify-center gap-2 hover:opacity-90 transition-all shadow-md">
-                  <Link to='/booking' className="font-semibold text-lg text-nowrap md:text-xl tracking-wide">Take Appointment Now</Link>
+                  <Link to='/booking' className="font-semibold text-2sm text-nowrap md:text-xl tracking-wide">Take Appointment Now</Link>
                 </button>
               </div>
             </div>
